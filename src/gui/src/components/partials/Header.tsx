a531cb44437d4258ba16fc01d0a66ccef48705a8
François Popieul <francois.popieul@hotmail.fr>
2026-05-13 14:50:46 +0200

    Accessibility fixes (#38)
    * fix: update button container styles for better alignment and responsiveness; correct blue color variable
    
    * fix: update link styles for buttons to improve consistency across components
    
    * fix: refactor navigation links to use useNavigate for improved routing; update button styles for consistency

--- src/gui/src/components/partials/Header.tsx
+++ src/gui/src/components/partials/Header.tsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 import Button from "../ui/Button";
 import "./Header.css";
 import Brand from "./Brand";
 import BurgerMenu from "../../homepage/components/BurgerMenu";
 
 function Header() {
+    const navigate = useNavigate();
+
     return (
         <header className="homepage_header">
             <Brand variant="dark" width="large" />
             <nav className="navigation_container">
                 <div className="button_container">
-                    <Link to="/login" className="button_link"><Button name="Se connecter" variant="light" width="medium" /></Link>
-                    <Link to="/signup" className="button_link"><Button name="Commencer" variant="blue" width="medium" /></Link>
+                    <Button name="Se connecter" variant="light" width="medium" onClick={() => navigate("/login")} />
+                    <Button name="Commencer" variant="blue" width="medium" onClick={() => navigate("/signup")} />
                 </div>
                 <BurgerMenu />
             </nav>
