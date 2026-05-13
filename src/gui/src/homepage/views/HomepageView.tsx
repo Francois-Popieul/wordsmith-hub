afaa0d176c2db8337c447495b184930408ff3ff0
François Popieul <francois.popieul@hotmail.fr>
2026-05-13 11:53:34 +0200

    feat: enhance homepage with call-to-action section and styling (#32)

--- src/gui/src/homepage/views/HomepageView.tsx
+++ src/gui/src/homepage/views/HomepageView.tsx
@@ -6,44 +6,54 @@ import "../../stylesheets/homepage.css";
 import image01 from "../../assets/image_01.avif";
 import image02 from "../../assets/image_02.avif";
 import { ClockIcon, CustomersIcon, LanguageIcon, ProjectsIcon } from "../../assets/icons/icons";
+import Button from "../../components/ui/Button";
+import { Link } from "react-router";
 
 function HomepageView() {
     return (<>
         <Header />
-        <main className="homepage">
-            <h1 className="visually-hidden">Page d’accueil de Wordsmith Hub, la plateforme de gestion pour les traducteurs indépendants</h1>
-            <section className="homepage_presentation_container">
-                <div className="homepage_presentation_text_container">
-                    <h2 className="homepage_presentation_title">Simplifiez la gestion de votre activité de traduction</h2>
-                    <p className="homepage_presentation_text">Wordsmith Hub est un service complet destiné aux traducteurs indépendants. Gérez vos clients, suivez vos projets et créez vos factures au même endroit.</p>
-                </div>
-                <div className="homepage_image_container">
-                    <img className="homepage_horizontal_image" src={image01} alt="Image d’illustration représentant un chat sur un clavier" fetchPriority="high" width={2000} height={1333} />
-                </div>
-            </section>
+        <main>
+            <section className="homepage">
+                <h1 className="visually-hidden">Page d’accueil de Wordsmith Hub, la plateforme de gestion pour les traducteurs indépendants</h1>
+                <section className="homepage_presentation_container">
+                    <div className="homepage_presentation_text_container">
+                        <h2 className="homepage_presentation_title">Simplifiez la gestion de votre activité de traduction</h2>
+                        <p className="homepage_presentation_text">Wordsmith Hub est un service complet destiné aux traducteurs indépendants. Gérez vos clients, suivez vos projets et créez vos factures au même endroit.</p>
+                    </div>
+                    <div className="homepage_image_container">
+                        <img className="homepage_horizontal_image" src={image01} alt="Image d’illustration représentant un chat sur un clavier" fetchPriority="high" width={2000} height={1333} />
+                    </div>
+                </section>
 
-            <section className="homepage_tools_container">
-                <h2 className="homepage_tools_title">Tout ce qu’il vous faut pour réussir</h2>
-                <p className="homepage_tools_text">Des outils pratiques conçus pour les traducteurs indépendants</p>
-                <div className="homepage_vignette_container">
-                    <HomepageVignette title="Gestion des clients" text="Enregistrez vos clients, leurs informations de contact et vos tarifs pour chaque paire de langue et vos différents services." icon={<CustomersIcon size={32} color="var(--color-blue-deep)" />} />
-                    <HomepageVignette title="Suivi des projets" text="Organisez vos projets de traduction en notant leur statut, les langues de travail, le client final, etc." icon={<ProjectsIcon size={32} color="var(--color-blue-deep)" />} />
-                    <HomepageVignette title="Gestion des commandes" text="Suivez votre charge de travail grâce aux informations détaillées sur chaque commande&nbsp;: compte de mots, tarifs, date de livraison, etc." icon={<ClockIcon size={32} color="var(--color-blue-deep)" />} />
-                    <HomepageVignette title="Prise en charge de plusieurs langues" text="Gérez facilement toutes les paires de langues. Configurez facilement vos tarifs pour toutes les combinaisons et tous les services." icon={<LanguageIcon size={32} color="var(--color-blue-deep)" />} />
-                </div>
-            </section>
+                <section className="homepage_tools_container">
+                    <h2 className="homepage_tools_title">Tout ce qu’il vous faut pour réussir</h2>
+                    <p className="homepage_tools_text">Des outils pratiques conçus pour les traducteurs indépendants</p>
+                    <div className="homepage_vignette_container">
+                        <HomepageVignette title="Gestion des clients" text="Enregistrez vos clients, leurs informations de contact et vos tarifs pour chaque paire de langue et vos différents services." icon={<CustomersIcon size={32} color="var(--color-blue-deep)" />} />
+                        <HomepageVignette title="Suivi des projets" text="Organisez vos projets de traduction en notant leur statut, les langues de travail, le client final, etc." icon={<ProjectsIcon size={32} color="var(--color-blue-deep)" />} />
+                        <HomepageVignette title="Gestion des commandes" text="Suivez votre charge de travail grâce aux informations détaillées sur chaque commande&nbsp;: compte de mots, tarifs, date de livraison, etc." icon={<ClockIcon size={32} color="var(--color-blue-deep)" />} />
+                        <HomepageVignette title="Prise en charge de plusieurs langues" text="Gérez facilement toutes les paires de langues. Configurez facilement vos tarifs pour toutes les combinaisons et tous les services." icon={<LanguageIcon size={32} color="var(--color-blue-deep)" />} />
+                    </div>
+                </section>
 
-            <section className="homepage_feature_container">
-                <div className="homepage_image_container">
-                    <img className="homepage_vertical_image" src={image02} alt="Image d’illustration représentant une bibliothèque" loading="lazy" width={1500} height={2250} />
-                </div>
-                <div>
-                    <h2 className="homepage_feature_title">Concentrez-vous sur la traduction, pas sur la gestion administrative</h2>
-                    <HomepageFeature title="Gagnez du temps" text="Réduisez votre charge administrative grâce à l’automatisation." />
-                    <HomepageFeature title="Faites-vous payer plus vite" text="Envoyez des factures professionnelles sitôt votre tâche achevée." />
-                    <HomepageFeature title="Développez votre activité" text="Prenez des décisions réfléchies grâce aux informations sur vos activités les plus rentables." />
-                    <HomepageFeature title="Optimisez votre organisation" text="Ne manquez jamais une échéance grâce à un système de gestion centralisée de vos commandes." />
-                </div>
+                <section className="homepage_feature_container">
+                    <div className="homepage_image_container">
+                        <img className="homepage_vertical_image" src={image02} alt="Image d’illustration représentant une bibliothèque" loading="lazy" width={1500} height={2250} />
+                    </div>
+                    <div>
+                        <h2 className="homepage_feature_title">Concentrez-vous sur la traduction, pas sur la gestion administrative</h2>
+                        <HomepageFeature title="Gagnez du temps" text="Réduisez votre charge administrative grâce à l’automatisation." />
+                        <HomepageFeature title="Faites-vous payer plus vite" text="Envoyez des factures professionnelles sitôt votre tâche achevée." />
+                        <HomepageFeature title="Développez votre activité" text="Prenez des décisions réfléchies grâce aux informations sur vos activités les plus rentables." />
+                        <HomepageFeature title="Optimisez votre organisation" text="Ne manquez jamais une échéance grâce à un système de gestion centralisée de vos commandes." />
+                    </div>
+                </section>
+
+            </section>
+            <section className="homepage_cta_container">
+                <h2 className="homepage_cta_title">Prêt à simplifier la gestion de votre activité de traduction&nbsp;?</h2>
+                <p className="homepage_cta_text">Rejoignez dès maintenant <strong>Wordsmith Hub</strong> pour découvrir comment transformer votre manière de travailler.</p>
+                <Link to={"/signup"}><Button name="Commencer gratuitement" width="default" variant="light" /></Link>
             </section>
         </main>
         <Footer />
